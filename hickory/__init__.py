660fbe268012d73beb5b98a1c7b620c4982b6715
Erin Sheldon <erin.sheldon@gmail.com>
2020-03-12 15:50:49 -0400

    initial commits

---
+++ hickory/__init__.py
@@ -0,0 +1,6 @@
+from . import plot  # noqa
+from .plot import Plot  # noqa
+
+from .data_containers import Points  # noqa
+from .data_containers import Function # noqa
+from . import data_containers  # noqa
---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import glob
+from setuptools import setup, find_packages
+
+setup(
+    name="hickory",
+    version="0.1.0",
+    packages=find_packages(),
+    author='Erin Sheldon',
+    author_email='erin.sheldon@gmail.com',
+    url='https://github.com/esheldon/hickory',
+)
