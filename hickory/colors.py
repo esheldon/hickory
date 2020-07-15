5be638533393f71590581d49702e8319f9059933
Erin Sheldon <erin.sheldon@gmail.com>
2020-07-14 23:10:35 -0400

    docs

--- hickory/colors.py
+++ hickory/colors.py
@@ -1,3 +1,7 @@
+"""
+color name to hex translation based on the rgb.txt -> hex conversion here
+https://github.com/timoxley/colornames
+"""
 def get_color(color_name):
     if color_name[0] == '#':
         return color_name
