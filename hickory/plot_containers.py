323fd1c92780e72324299b1a396a80d8161cf39a
Erin Sheldon <erin.sheldon@gmail.com>
2020-07-15 15:05:56 -0400

    add method to show without writing a file
    writes to an io.BytesIO buffer and converts to array,
    then to PIL Image

--- hickory/plot_containers.py
+++ hickory/plot_containers.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tempfile
 import matplotlib.pyplot as plt
 
@@ -32,14 +33,42 @@ class Plot(object):
 
     def write(self, fname, dpi=None):
         fig, ax = self.get_fig()
-        self.fig.savefig(fname, dpi=dpi)
+        if dpi is not None:
+            fig.set_dpi(dpi)
+
+        self.fig.savefig(fname)
 
     def show(self, dpi=None):
+        import io
+
+        fig, ax = self.get_fig()
+        # fig.canvas.draw()
+
+        if dpi is not None:
+            fig.set_dpi(dpi)
+
+        io_buf = io.BytesIO()
+        fig.savefig(io_buf, format='raw')
+        io_buf.seek(0)
+
+        shape = (int(fig.bbox.bounds[3]), int(fig.bbox.bounds[2]), -1)
+
+        data = np.frombuffer(io_buf.getvalue(), dtype=np.uint8)
+        img_array = np.reshape(
+            data,
+            newshape=shape,
+        )
+
+        io_buf.close()
+
+        _show_array_tkinter(img_array)
+
+    def _show_from_file(self, dpi=None):
         with tempfile.TemporaryDirectory() as dir:
             fname = tempfile.mktemp(dir=dir, suffix='.png')
             self.write(fname, dpi=dpi)
 
-            _show_tkinter(fname)
+            _show_file_tkinter(fname)
 
     def get_fig(self):
         if self.fig is None or self.ax is None:
@@ -88,7 +117,40 @@ def _show_viewer(fname, viewer='feh'):
     subprocess.check_call(command)
 
 
-class _TkinterWindow(object):
+class _TkinterWindowFromArray(object):
+    def __init__(self, img_array):
+        from tkinter import Tk, Canvas, NW
+        from PIL import ImageTk, Image
+
+        img = Image.fromarray(img_array)
+        w, h = img.size
+
+        self.root = Tk()
+        self.root.bind('q', self.destroy)
+
+        canvas = Canvas(self.root, width=w, height=h)
+        canvas.pack()
+
+        self.imgtk = ImageTk.PhotoImage(img)
+        canvas.create_image(0, 0, anchor=NW, image=self.imgtk)
+        self.root.mainloop()
+
+    def destroy(self, even):
+        self.root.destroy()
+
+
+def _show_array_tkinter(img_array):
+    """
+    requires pillow
+    """
+
+    try:
+        _ = _TkinterWindowFromArray(img_array)
+    except KeyboardInterrupt:
+        pass
+
+
+class _TkinterWindowFromFile(object):
     def __init__(self, fname):
         from tkinter import Tk, Canvas, NW
         from PIL import ImageTk, Image
@@ -110,12 +172,12 @@ class _TkinterWindow(object):
         self.root.destroy()
 
 
-def _show_tkinter(fname):
+def _show_file_tkinter(fname):
     """
     requires pillow
     """
 
     try:
-        _ = _TkinterWindow(fname)
+        _ = _TkinterWindowFromFile(fname)
     except KeyboardInterrupt:
         pass
