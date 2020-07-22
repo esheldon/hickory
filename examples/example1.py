dcb4f4414a518d19f0e02d1df2115d3a36741444
Erin Sheldon <erin.sheldon@gmail.com>
2020-07-22 07:39:41 -0400

    added example 1

---
+++ examples/example1.py
@@ -0,0 +1,58 @@
+"""
+Make a Plot with various markers and lines.  Use cyclers
+to choose colors, lines, and markers
+"""
+
+import numpy as np
+from hickory import (
+    Plot,
+    Legend,
+    get_marker_cycler,
+    get_line_cycler,
+    get_color_cycler,
+)
+
+
+def main():
+
+    err = 0.03
+    n = 10
+    x = np.linspace(0, 1, n)
+    yerr = x*0 + err
+
+    xlabel = r'$r ~[h^{-1}~\mathrm{Mpc}]$'
+    ylabel = r'$\Delta\Sigma ~[h~\mathrm{M}_\odot]$'
+    plt = Plot(
+        xlabel=xlabel,
+        ylabel=ylabel,
+        legend=Legend(loc='upper left'),
+    )
+
+    lcycler = get_line_cycler()
+    mcycler = get_marker_cycler()
+    ccycler = get_color_cycler()
+
+    for p in [1, 2, 3, 4]:
+        dlab = r'$\mathrm{data%d}$' % p
+        clab = r'$y = x^%d$' % p
+
+        ytrue = x**p
+        y = ytrue
+
+        plt.errorbar(
+            x, y, yerr=yerr, label=dlab,
+            marker=next(mcycler),
+            color=next(ccycler),
+        )
+        plt.plot(
+            x, ytrue,
+            label=clab,
+            linestyle=next(lcycler),
+            color=next(ccycler),
+        )
+    plt.axhline(0, color='black', linewidth=1)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
