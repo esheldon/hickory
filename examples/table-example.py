d7bb03c0415438c25b601a1a4d8f3c686a268ec8
Erin Sheldon <erin.sheldon@gmail.com>
2020-07-22 10:58:14 -0400

    working on examples

--- examples/table-example.py
+++ examples/table-example.py
@@ -1,51 +1,45 @@
+"""
+basic table example
+"""
 import numpy as np
 from hickory import Table, get_color_cycler
 
 
-def main():
-    seed = 8312
-    rng = np.random.RandomState(seed)
+seed = 8312
+rng = np.random.RandomState(seed)
 
-    tab = Table(
-        figsize=[6.4, 6.4],
-        nrows=2, ncols=2,
+tab = Table(
+    figsize=[6.4, 6.4],
+    nrows=2, ncols=2,
+)
+
+ccycler = get_color_cycler()
+
+n = 40
+x = np.linspace(-1, 1, n)
+err = 0.3
+
+# iterate over subplots
+for i, plt in enumerate(tab):
+    pindex = i+1
+
+    ytrue = x**pindex
+    y = ytrue + rng.normal(scale=err, size=n)
+    yerr = ytrue*0 + err
+
+    plt.errorbar(
+        x, y, yerr=yerr, color=next(ccycler),
+        markeredgecolor='black',
     )
+    plt.plot(
+        x, ytrue, marker=None, linestyle='-',
+        color=next(ccycler),
+    )
+    plt.set(
+        xlabel=r'$x [\mathrm{cm}]$',
+        ylabel=r'$x^%d$' % pindex,
+        margin=0.2,
+    )
+    plt.set_aratio(1.0)
 
-    ccycler = get_color_cycler()
-
-    n = 40
-    x = np.linspace(-1, 1, n)
-    err = 0.3
-
-    # row, col, power law index
-    indexes = [
-        (0, 0, 1),
-        (0, 1, 2),
-        (1, 0, 3),
-        (1, 1, 4),
-    ]
-
-    for ti in indexes:
-        row, col, pindex = ti
-
-        ytrue = x**pindex
-        y = ytrue + rng.normal(scale=err, size=n)
-        yerr = ytrue*0 + err
-
-        plt = tab[row, col]
-        plt.errorbar(x, y, yerr=yerr, color=next(ccycler))
-        plt.plot(
-            x, ytrue, marker=None, linestyle='-',
-            color=next(ccycler),
-        )
-        plt.set(
-            xlabel=r'$x [\mathrm{cm}]$',
-            ylabel=r'$x^%d$' % pindex,
-        )
-        plt.set_aratio(1.0)
-
-    tab.show()
-
-
-if __name__ == '__main__':
-    main()
+tab.show()
