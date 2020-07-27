6795623f935b551ca3bc0e7259fdb63554d02801
Erin Sheldon <erin.sheldon@gmail.com>
2020-07-27 14:41:56 -0400

    defautlts in config for show/fork depend on if in ipython/jupyter

--- hickory/configuration.py
+++ hickory/configuration.py
@@ -1,8 +1,15 @@
+try:
+    import IPython
+    _name = IPython.get_ipython().__class__.__name__
+except ImportError:
+    _name = None
+
 config = {
-    # if set, show the plot by default in the convenience functions
-    'show': False,
+    # by default plot, plot_hist etc. convenience functions show
+    # on the screen, except in jupyter
+    'show': False if _name == 'ZMQInteractiveShell' else True,
 
     # if set, the convenience functions put the plot into
     # the background by default
-    'fork_window': False,
+    'fork_window': True if _name == 'TerminalInteractiveShell' else False,
 }
